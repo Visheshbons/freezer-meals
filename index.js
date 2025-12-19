fd4b03f55efa6089a85975dce44a25c21b6561b7
Vishesh Kudva <vishesh.kudva@outlook.com>
2025-12-19 15:53:52 +1300

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
